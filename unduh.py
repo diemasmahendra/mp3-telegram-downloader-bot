6547966a626725fdede4b1ed42f7f72153915c53
asmintamvan <asmin987asmin@gmail.com>
2021-01-27 01:04:48 +0800

    firsttgdhdftswasaaqa

--- unduh.py
+++ unduh.py
@@ -27,23 +27,10 @@ class Main:
     def get_source(self, raw_link, filename):
         url = "https://michaelbelgium.me/ytconverter/convert.php?youtubelink=https://www.youtube.com/watch?v="
         url = requests.get(url + raw_link).json().get('file')
-        print('url:', url)
         if url:
-            with open(filename, 'wb') as f:
-                response = requests.get(url, stream=True)
-                total = response.headers.get('content-length')
-                print(total)
-                if total is None:
-                    return False
-                else:
-                    downloaded = 0
-                    total = int(total)
-                    for data in response.iter_content(chunk_size=max(int(total/1000), 1024*1024)):
-                        downloaded += len(data)
-                        f.write(data)
-                        done = int(50*downloaded/total)
-                        sys.stdout.write('\r[{}{}]'.format('█' * done, '.' * (50-done)))
-                        sys.stdout.flush()
-                        return True
+            with open(filename, "wb") as f:
+                response = requests.get(url)
+                f.write(response.content)
+                return True
         else:
             return False
